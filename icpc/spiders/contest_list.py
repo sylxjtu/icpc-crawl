19a54169b8c2a9a1be72ff7b3656e4ffb3affa8b
sylxjtu <a443833359@gmail.com>
2018-12-01 00:56:23 +0800

    add data source

--- icpc/spiders/contest_list.py
+++ icpc/spiders/contest_list.py
@@ -4,6 +4,7 @@ import os
 
 include_list = ["Regional"]
 exclude_list = ["First Round", "Preliminary"]
+max_team = 5000
 
 def inc_exc_filter(s):
     for inc in include_list:
@@ -46,13 +47,32 @@ class ContestListSpider(scrapy.Spider):
         print("ids =", *contest_ids, sep = '\n')
         for abbr, contest_id in contest_ids:
             yield scrapy.Request(
-                "https://icpc.baylor.edu/cm5-contest-rest/rest/contest/standings/contest/{}?q=proj:place,institution,team,problemsSolved,totalTime,lastSolution%3Bsort:place+asc%3B&page=1&size=5000".format(contest_id),
+                "https://icpc.baylor.edu/cm5-contest-rest/rest/contest/standings/contest/{}?q=proj:place,institution,team,problemsSolved,totalTime,lastSolution%3Bsort:place+asc%3B&page=1&size={}".format(contest_id, max_team),
                 callback=self.parse_standing,
+                meta={"abbr": abbr}
+            )
+            yield scrapy.Request(
+                "https://icpc.baylor.edu/cm5-contest-rest/rest/contest/public/{}-{}".format(abbr, int(self.year) - 1),
+                callback=self.parse_site,
                 meta={"abbr": abbr}
             )
 
     def parse_standing(self, response):
-        with open(self.year + "/" + response.meta["abbr"] + ".json", "wb") as f:
+        with open(self.year + "/" + response.meta["abbr"] + ".standing.json", "wb") as f:
             f.write(response.body)
 
-        
+    def parse_site(self, response):
+        data = json.loads(response.body_as_unicode())
+        if len(data["sites"]) != 1:
+            print("unexpected len(data['sites']) of", response.meta["abbr"], ":", len(data["sites"]))
+        else:
+            site_id = data["sites"][0]["id"]
+            yield scrapy.Request(
+                "https://icpc.baylor.edu/cm5-team/rest/team/table/site/accepted/{}?q=proj:institution,country,name,status%3B&page=1&size={}".format(site_id, max_team),
+                callback=self.parse_teams,
+                meta=response.meta
+            )
+
+    def parse_teams(self, response):
+        with open(self.year + "/" + response.meta["abbr"] + ".teams.json", "wb") as f:
+            f.write(response.body)
--- process.py
+++ process.py
@@ -4,11 +4,30 @@ import json
 
 # config
 ignore_institution = False
-ignore_zero_solve = True
+# Strict mode, only teams really participated (solved problems) will be shown
+strict_mode = False
 # Urumqi (2017) is ignored due to contest time adjustment and many teams had quited or changed team member
 ignore_contest = ["Urumqi"]
 max_contest = 3
 # ~config
+
+class TeamOfContest:
+    def __init__(self, name, solved):
+        self.name = name
+        self.solved = solved
+
+    def __eq__(self, rhs):
+        return self.name == rhs.name
+
+    def __hash__(self):
+        return self.name.__hash__()
+
+    def __str__(self):
+        return self.name + ", solved " + str(self.solved)
+
+    def __repr__(self):
+        return self.__class__.__name__ + "(" + self.name + ", " + str(self.solved) + ")"
+
 
 team_count = {}
 year = sys.argv[1]
@@ -19,17 +38,33 @@ for fn in os.listdir(year):
             break
     else:
         with open(os.path.join(year, fn), "rb") as f:
-            data = json.loads(f.read())
-            for team in data:
-                if team.get("problemsSolved", -1) == 0 and ignore_zero_solve:
-                    continue
-                team_id = ""
-                if not ignore_institution:
-                    team_id = team["institution"].strip() + "+" + team["team"].strip()
-                else:
-                    team_id = team["team"].strip()
-                s = team_count.get(team_id, set())
-                s.add('{},solve{}'.format(fn[:-5], team.get("problemsSolved", "unknown")))
-                team_count[team_id] = s
+            if 'standing' in fn:
+                data = json.loads(f.read())
+                for team in data:
+                    if team.get("problemsSolved", -1) <= 0 and strict_mode:
+                        continue
+                    team_id = ""
+                    if not ignore_institution:
+                        team_id = team["institution"].strip() + "+" + team["team"].strip()
+                    else:
+                        team_id = team["team"].strip()
+                    s = team_count.get(team_id, set())
+                    s.add(TeamOfContest(fn.split('.')[0], team.get("problemsSolved", -1)))
+                    team_count[team_id] = s
+            elif 'teams' in fn:
+                if strict_mode: continue
+                data = json.loads(f.read())
+                for team in data:
+                    if team.get("status", "UNKNOWN") != "ACCEPTED":
+                        continue
+                    team_id = ""
+                    if not ignore_institution:
+                        team_id = team["institution"].strip() + "+" + team["name"].strip()
+                    else:
+                        team_id = team["name"].strip()
+                    s = team_count.get(team_id, set())
+                    if TeamOfContest(fn.split('.')[0], -1) in s: continue
+                    s.add(TeamOfContest(fn.split('.')[0], -1))
+                    team_count[team_id] = s
 
 print(*[(team, team_count[team]) for team in team_count if len(team_count[team]) >= max_contest], sep = '\n')
